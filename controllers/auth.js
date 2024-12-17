007bb609585829c13c4c2978892ec6f85fa1fec3
AwaabElamin <AwaabElamin@gmail.com>
2024-12-17 13:27:59 -0600

    update login feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,40 @@
 const JwtManager = require('../jwt/jwtManager');
 const bcrypt = require('bcrypt');
 const userModel = require('../models/users');
-
+const connectUsersDB = require('mongoose');
+const connectionString =
+    'mongodb+srv://root:123@cluster0.wpzy5.mongodb.net/Delivery?retryWrites=true&w=majority';
 exports.login = async (req, res) => {
     console.log("login controller");
     const { email, password } = req.body;
     console.log(`username: ${email}, password: ${password}`);
-    let user = await userModel.findByEmail(email);
-    if (email, password) {
-        if (user) {
-            console.log('user', user);
-            if (bcrypt.compareSync(password, user.password)) {
-                const data = { email: email, password: password };
-                const jwt = new JwtManager();
-                const token = jwt.generate(data);
-                console.log("Token: ", token);
-                res.json({ status: 'success', accessToken: token, email: email, _id: user._id });
+    await connectUsersDB.connect(connectionString)
+        .then(async () => {
+            let user = await userModel.findByEmail(email);
+            if (email, password) {
+                if (user) {
+                    console.log('user', user);
+                    if (bcrypt.compareSync(password, user.password)) {
+                        const data = { email: email, password: password };
+                        const jwt = new JwtManager();
+                        const token = jwt.generate(data);
+                        console.log("Token: ", token);
+                        res.json({ status: 'success', accessToken: token, email: email, _id: user._id });
+                    } else {
+                        res.send({ status: 'fail', message: 'Wrong password' });
+                    }
+                } else {
+                    res.send({ status: 'fail', message: 'The user is not found' });
+                }
             } else {
-                res.send({ status: 'fail', message: 'Wrong password' });
+                res.send({ status: 'fail', data: 'Please provide the email and password' });
             }
-        } else {
-            res.send({ status: 'fail', message: 'The user is not found' });
-        }
-    } else {
-        res.send({ status: 'fail', data: 'Please provide the email and password' });
-    }
+        })
+        .catch(error => {
+            console.log("Route Users login catch connected", error);
+            res.send({ status: "error", data: error })
+        });
+
 }
 exports.authorize = (req, res, next) => {
     if (req.headers.authorization) {
@@ -51,8 +61,8 @@ exports.authorize = (req, res, next) => {
         //         next();
         //         }
         // })
-        } else {
-            res.send({ status: 'fail', message: 'Unauthenticated' });
-        }
-    
+    } else {
+        res.send({ status: 'fail', message: 'Unauthenticated' });
+    }
+
 }
