5d23b4a2227732c1ce42e759c59a871b33e98ec2
AwaabElamin <AwaabElamin@gmail.com>
2024-12-24 12:04:53 -0600

    update users connection to DB

--- models/users.js
+++ models/users.js
@@ -1,14 +1,18 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const connectToUsersDB = require('mongoose');
+const connectionString =
+    'mongodb+srv://root:123@cluster0.wpzy5.mongodb.net/users?retryWrites=true&w=majority';
+
 const userSchema = connectToUsersDB.Schema({
     email: { type: String, unique: true },
     password: { type: String },
     phone: { type: String },
-    firstname:{type: String},
-    lastname: {type: String}
+    firstname: { type: String },
+    lastname: { type: String }
 })
 const userModel = connectToUsersDB.model('users', userSchema);
+const conn = connectToUsersDB.createConnection(connectionString);
 class UserCollection {
     static async findAll() {
         const users = await userModel.find({});
@@ -21,31 +25,46 @@ class UserCollection {
         return user;
     }
     static async create(user) {
+        console.log('error', conn);
+        // return conn.connections;
         const new_user = new userModel(user);
         try {
             await new_user.save();
-            console.log('create:- ', new_user);
-            return { success: true, data: new_user };
-        } catch (error) {
-            console.log('Awaab.code: ', error.code);
-            if (error.code == 11000) {
-                return { success: false, message: "users already exist" };
-            } else {
-                return { success: false, message: error };
-            }
+            return new_user;
         }
+        catch (error) {
+            if (error.code == 11000) {
+                return "users already exist";
+            } else {
+                return error;
+            }
+        };
+
+
+        // try {
+        //     await new_user.save();
+        //     console.log('create:- ', new_user);
+        //     return { success: true, data: new_user };
+        // } catch (error) {
+        //     console.log('Awaab.code: ', error.code);
+        //     if (error.code == 11000) {
+        //         return { success: false, message: "users already exist" };
+        //     } else {
+        //         return { success: false, message: error };
+        //     }
+        // }
     }
     static async findAndUpdate(id, user) {
         console.log('id: ', id);
         try {
-            const foundedUser = await userModel.findOne({ email:id});
-            if(foundedUser) {
+            const foundedUser = await userModel.findOne({ email: id });
+            if (foundedUser) {
                 const updatedUser = await userModel.findByIdAndUpdate(foundedUser._id, user);
                 console.log('findAndUpdate:- ', updatedUser);
                 return { status: 'success', data: updatedUser };
             }
-            return {status:'fail', message:'user not founded by email: '+id}
-            
+            return { status: 'fail', message: 'user not founded by email: ' + id }
+
         } catch (error) {
             return { status: 'fail', message: error.message };
 
