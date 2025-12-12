a3d4a228e4e3b4bfa5a662fbf85597a7f3abe9c9
AwaabElamin <AwaabElamin@gmail.com>
2025-12-12 16:05:31 -0600

    chore(main): remove users, auto, resume features (moved to branches)

--- routes/auto.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auto = require('../controllers/auto');
-router.get('/user/:email',auto.getAutoUser);
-router.post('/user',auto.addAutoUser);
-module.exports = router;
