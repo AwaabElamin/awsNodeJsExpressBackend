96b196ea2a6e4adabd5b9b7ff61c1f6d445e7e50
AwaabElamin <AwaabElamin@gmail.com>
2025-12-12 15:40:38 -0600

    chore(get-projects): update app.js to only mount mainMain, auth, health

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ var cookieParser = require('cookie-parser');
 var path = require('path');
 var logger = require('morgan');
 const authorizeRouter = require('./routes/auth');
-const usersRouter = require('./routes/users');
 const mainMainRouter = require('./routes/mainMain');
-const resumeRoute = require('./routes/resume');
-const autoRouter = require('./routes/auto');
 const healthRouter = require('./routes/health');
 
 var app = express();
@@ -23,10 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use('/', authorizeRouter);
-app.use('/users', usersRouter);
-app.use('/resume', resumeRoute);
 app.use('/mainMain', mainMainRouter);
-app.use('/auto',autoRouter)
 app.use('/health', healthRouter);
 
 // error handler
