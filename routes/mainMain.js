3a4ec232785d24bc4e1aee61bd9cb160ab835a84
AwaabElamin <AwaabElamin@gmail.com>
2025-12-12 15:41:52 -0600

    chore(main): remove mainMain feature, moved to get-projects branch

--- routes/mainMain.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const mainMainController = require('../controllers/mainMain');
-router.get('',mainMainController.getProjects);
-router.post('',mainMainController.create);
-module.exports = router;
