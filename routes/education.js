ba79836e1103e6257a999d6ae6518cded9e23b71
AwaabElamin <AwaabElamin@gmail.com>
2025-12-19 11:54:39 -0600

    update education

--- routes/education.js
+++ routes/education.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const authorizationController = require('../controllers/auth');
 const educationController = require('../controllers/education');
 
-
 // Create education for authenticated user
 router.post('/', authorizationController.authorize, educationController.createEducation);
 // Get all educations for authenticated user
